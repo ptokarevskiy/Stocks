78dd5f0c0ffeb743c4c80473d5fa67007cf3a226
Pavel Tokarevskiy <p.tokarevskiy@corp.vk.com>
2021-08-08 12:25:30 +0300

    SearchResults + Delegate

--- Stocks/Controllers/SearchResultsViewController.swift
+++ Stocks/Controllers/SearchResultsViewController.swift
@@ -1,19 +1,89 @@
 import UIKit
 
+// MARK: - SearchResultsViewControllerDelegate
+
+protocol SearchResultsViewControllerDelegate: AnyObject {
+    func searchResultsViewControllerDidSelect(searchResult: String)
+}
+
+// MARK: - SearchResultsViewController
+
 class SearchResultsViewController: UIViewController {
+    weak var delegate: SearchResultsViewControllerDelegate?
+
+    private var results: [String] = []
+
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(SearchResultTableViewCell.self,
+                       forCellReuseIdentifier: SearchResultTableViewCell.identifier)
+
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setUpTableView()
     }
 
-    /*
-     // MARK: - Navigation
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
+        tableView.frame = view.bounds
+    }
+
+    // MARK: - Private
+
+    private func setUpTableView() {
+        view.addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    // MARK: - Public
+
+    public func update(withResults results: [String]) {
+        self.results = results
+        tableView.reloadData()
+    }
 }
+
+// MARK: UITableViewDelegate, UITableViewDataSource
+
+extension SearchResultsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchResultTableViewCell.identifier, for: indexPath)
+
+        cell.textLabel?.text = "AAPL"
+        cell.detailTextLabel?.text = "Apple"
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        delegate?.searchResultsViewControllerDidSelect(searchResult: "APPL")
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+    import SwiftUI
+
+    @available(iOS 14.0, *)
+    struct SearchResultsViewController_Preview: PreviewProvider {
+        static var previews: some View {
+            UIViewControllerPreview {
+                UINavigationController(rootViewController: SearchResultsViewController())
+            }.previewLayout(.sizeThatFits)
+                .environment(\.colorScheme, .light)
+                .ignoresSafeArea(.all)
+        }
+    }
+#endif
--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -1,10 +1,77 @@
 import UIKit
 
+// MARK: - WatchListViewController
+
 class WatchListViewController: UIViewController {
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .systemBackground
-        
+
+        setUpSearchController()
+        setUpTitleView()
+    }
+
+    // MARK: - Private
+
+    private func setUpSearchController() {
+        let resultsViewController = SearchResultsViewController()
+        let searchViewController = UISearchController(searchResultsController: resultsViewController)
+
+        resultsViewController.delegate = self
+        searchViewController.searchResultsUpdater = self
+        navigationItem.searchController = searchViewController
+    }
+
+    private func setUpTitleView() {
+        let titleView: UIView = .init(frame: .init(x: 0,
+                                                   y: 0,
+                                                   width: view.width,
+                                                   height: navigationController?.navigationBar.height ?? 100))
+        let label = UILabel(frame: .init(x: 10, y: 0, width: titleView.width - 20, height: titleView.height))
+        label.text = "Stocks"
+        label.font = .systemFont(ofSize: 38, weight: .medium)
+        titleView.addSubview(label)
+        navigationItem.titleView = titleView
     }
 }
+
+// MARK: UISearchResultsUpdating
+
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let resultsViewController = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+
+        // FIXME: Call API + add delay
+        resultsViewController.update(withResults: ["GOOG"])
+    }
+}
+
+// MARK: SearchResultsViewControllerDelegate
+
+extension WatchListViewController: SearchResultsViewControllerDelegate {
+    func searchResultsViewControllerDidSelect(searchResult: String) {
+        // Present stock for selection
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+    import SwiftUI
+
+    @available(iOS 14.0, *)
+    struct WatchListViewController_Preview: PreviewProvider {
+        static var previews: some View {
+            UIViewControllerPreview {
+                UINavigationController(rootViewController: WatchListViewController())
+            }.previewLayout(.sizeThatFits)
+                .environment(\.colorScheme, .light)
+                .ignoresSafeArea(.all)
+        }
+    }
+#endif
